c5c119d914d0d26b4690d26a1586be862c4ff039
Jiawen Yan <yjw9100@hotmail.com>
2018-04-02 11:36:24 +0800

    Add files via upload

---
+++ Week1/hour1.py
@@ -0,0 +1,48 @@
+# print function and define a varible
+
+a = 1
+print(a)
+b = "Today is good, and Python is great"
+b = 'Today is good, and Python is great'
+print(b)
+
+
+# Math Operations 
+a = 1
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) # Bang!
+
+print(1.0*a/b)
+print(a**2)
+
+import math
+# see more at https://docs.python.org/3/library/math.html
+print(math.sqrt(4))
+print(math.floor(4.6))
+print(math.ceil(4.6))
+print(math.log(4)) # natural log 
+print(math.log2(4)) # log base of 2
+print(math.log10(4)) # log base of 10
+
+print(math.pow(2,3))
+print(2**3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
